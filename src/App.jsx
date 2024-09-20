36ca9f787ab23642ac54f42751e0f5637b50c760
Anil <ak26122k@gmail.com>
2024-09-20 17:12:45 +0530

    everything work properly but version detail page give me error in after deploy but in development it works properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import PackageDetail from "./pages/PackageDetail";
-import VersionDetailPage from "./pages/VersionDetailPage";
+import SpecialVersionPage from "./pages/SpecialVersionPage";
+// import VersionDetailPage from "./pages/VersionDetailPage";
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/package/:name" element={<PackageDetail />} />
-      <Route path="/package/:name/:version" element={<VersionDetailPage />} />
+      {/* <Route path="/package/:name/:version" element={<VersionDetailPage />} /> */}
+      <Route path="/package/:name/:version" element={<SpecialVersionPage />} />
     </Routes>
   </Router>
 );

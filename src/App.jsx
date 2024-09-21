95b1d02b4e8761293cb2b7537c6bfaa7216902da
Anil <ak26122k@gmail.com>
2024-09-21 05:39:12 +0530

    i think problem solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import PackageDetail from "./pages/PackageDetail";
-import SpecialVersionPage from "./pages/SpecialVersionPage";
-// import VersionDetailPage from "./pages/VersionDetailPage";
+
+import VersionDetailPage from "./pages/VersionDetailPage";
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/package/:name" element={<PackageDetail />} />
-      {/* <Route path="/package/:name/:version" element={<VersionDetailPage />} /> */}
-      <Route path="/package/:name/:version" element={<SpecialVersionPage />} />
+      <Route path="/package/:name/:version" element={<VersionDetailPage />} />
     </Routes>
   </Router>
 );
